23b3b7eb990a0d6b7b9ef03e076a2f4340661932
brosie224 <barosengarten@gmail.com>
2019-07-12 16:25:39 -0400

    Added js view player to index page

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -6,17 +6,30 @@ $(function nextPlayer() {
     $("#js-next-player").on("click", function(e) {
         e.preventDefault();
         let nextId = parseInt($("#js-next-player").attr("data-id")) + 1;
+        // let nextId = $(this).data("id") + 1; -- doesn't work
         $.get("/players/" + nextId + ".json", function(data) {
             let player = data;
             $("#player-name-position").html(player["full_name"] + " - " + player["position"]);
-            $("#player-team").html(`<a href="/users/${player["user"]["id"]}">${player["team"]["full_name"]}</a>`)
-            $("#player-espn").html(`<a href="${player["espn_page"]}" target="_blank">ESPN Player Page</a>`)
+            $("#player-team").html(`<a href="/users/${player["user"]["id"]}">${player["team"]["full_name"]}</a>`);
+            $("#player-espn").html(`<a href="${player["espn_page"]}" target="_blank">ESPN Player Page</a>`);
             $("#js-next-player").attr("data-id", player["id"]);
         });
     });
 });
 
-
+$(function viewPlayer() {
+    $(".js-view-player").on("click", function(e) {
+        e.preventDefault();
+        // let thisId = parseInt($(".js-view-player").attr("data-id")) -- doesn't work
+        let thisId = $(this).data("id");
+        $.get("/players/" + thisId + ".json", function(data) {
+            let clickedPlayer = new Player(data);
+            let postPlayer = clickedPlayer.postHTML();
+            $("#player-team-info").html(postPlayer);
+            // document.getElementById('player-team-info').innerHTML = postPlayer;
+        })
+    })
+})
 
 class Player {
     constructor(obj) {
@@ -30,18 +43,20 @@ class Player {
 }
 
 Player.prototype.fullName = function() {
-    return this.firstName + this.lastName
+    return this.firstName + " " + this.lastName;
+}
+
+Player.prototype.teamName = function() {
+    return this.team.full_name;
 }
 
 Player.prototype.postHTML = function() {
-    return(`
+    return `
         <div>
-            <h3>${this.fullName}</h3>
+            <h2>${this.fullName()}</h2>
             <p>Position: ${this.position}</p>
-            <a href="${this.espnPage}">ESPN Player Page</a>
+            <p>${this.teamName()}</p>
+            <a href="${this.espnPage}" target="_blank">ESPN Player Page</a>
         </div>
-    `)
+    `
 }
-
-// need (` ?    or can do ` alone
-// does prototype fullName need 'return' ?
